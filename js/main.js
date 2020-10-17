40527b3278912605b5178a26c3da3d479c67b96c
rault <raultejeromartos@gmail.com>
2020-10-17 20:57:00 +0200

    ´pocos cambio

--- js/main.js
+++ js/main.js
@@ -5,20 +5,6 @@ let priorityAdd = document.querySelector('#select_header');
 let inputFilter = document.querySelector('#input_main');
 let btnFind = document.querySelector('#select_main');
 
-
-console.log(list);
-
-
-
-// function print(pList, pSection) {
-//     pList.forEach(el => {
-//         pSection.innerHTML += `<li>${el.titulo}<button>Eliminar</button></li>`;
-//     });
-// }
-
-// print(list, sectionList);
-
-
 let counterId = 0;
 function addTask() {
     if (inputAdd.value != "" && priorityAdd.value != "") {
@@ -30,17 +16,12 @@ function addTask() {
         counterId++
         inputAdd.value = "";
         priorityAdd.value = "";
-        console.log(list);
+
     } else {
         alert('No pueden estar los campos vacios');
         inputAdd.value = "";
         priorityAdd.value = "";
     }
-
 }
 
 btnAdd.addEventListener('click', addTask)
-
-
-
-
