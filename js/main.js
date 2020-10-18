afcddd9c1b30c7a89bdaff6ca9f163413146bfb7
rault <raultejeromartos@gmail.com>
2020-10-18 19:19:18 +0200

    mas cambios en css chulos, falta filtrar

--- js/main.js
+++ js/main.js
@@ -11,11 +11,11 @@ function print(pList, pSection = sectionList) {
     pSection.innerHTML = "";
     pList.forEach(el => {
         if (el.prioridad == 'diario') {
-            pSection.innerHTML += `<li class ="priority_red" data-id="${el.idTarea}">${el.titulo}<button onClick="removeTask(this)">Eliminar</button></li>`;
+            pSection.innerHTML += `<li class ="priority_red" data-id="${el.idTarea}">${el.titulo}<button onClick="removeTask(this)"><i class="fas fa-eraser"></i></button></li>`;
         } else if (el.prioridad == 'semanal') {
-            pSection.innerHTML += `<li class ="priority_yellow"data-id="${el.idTarea}">${el.titulo}<button onClick="removeTask(this)">Eliminar</button></li>`;
+            pSection.innerHTML += `<li class ="priority_yellow"data-id="${el.idTarea}">${el.titulo}<button onClick="removeTask(this)"><i class="fas fa-eraser"></i></button></li>`;
         } else {
-            pSection.innerHTML += `<li class ="priority_green"data-id="${el.idTarea}">${el.titulo}<button onClick="removeTask(this)">Eliminar</button></li>`;
+            pSection.innerHTML += `<li class ="priority_green"data-id="${el.idTarea}">${el.titulo}<button onClick="removeTask(this)"><i class="fas fa-eraser"></i></button></li>`;
         };
 
     });
@@ -72,6 +72,7 @@ function removeTask(event) {
     let id = event.parentNode.dataset.id;
     remove(list, id);
 };
+
 
 
 
